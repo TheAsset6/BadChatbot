89a53dbe212732be110686e181ff831860c997c4
TheAsset6 <ktsj20@gmail.com>
2023-07-08 12:45:38 -0400

    duff badrobot

---
+++ badRobot.py
@@ -0,0 +1,85 @@
+#import necessary libraries
+import io
+import random
+import string # process python strings
+import warnings
+# import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+warnings.filterwarnings('ignore')
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+nltk.download('popular', quiet=True) # for downloading packages
+
+# uncomment the following only the first time
+#nltk.download('punkt') # first-time use only
+#nltk.download('wordnet') # first-time use only
+
+
+#Reading in the corpus
+with open('badAdvice.txt','r', encoding='utf8', errors ='ignore') as fin:
+    raw = fin.read().lower()
+
+#Tokenisation
+sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
+word_tokens = nltk.word_tokenize(raw)# converts to list of words
+
+# Preprocessing
+lemmer = WordNetLemmatizer()
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+# Keyword Matching
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey","yo","wassup", "aye", "what up")
+GREETING_RESPONSES = ["hi", "yoooo", "hey", "*nods*", "hi there", "hello", "You talking to me?", "hey sunshine", "yao", "what's crack'n", "hey boo", "wuzzzz uuuuupppp","aye", "what up", "*winks*", "what up doe"]
+
+def greeting(sentence):
+    """If user's input is a greeting, return a greeting response"""
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+
+# Generating response
+def response(user_response):
+    robo_response=''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        robo_response=robo_response+"I am sorry, I don't understand you. Speak louder!"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
+
+
+flag=True
+print("Hi, my name is Duff the robot and I give bad advice.")
+while(flag==True):
+    user_response = input()
+    user_response=user_response.lower()
+    if(user_response!='bye'):
+        if(user_response=='thanks' or user_response=='thank you' ):
+            flag=False
+            print("good riddens")
+        else:
+            if(greeting(user_response)!=None):
+                print("Duff: "+greeting(user_response))
+            else:
+                print("Duff: "+response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("Yep. Kick Rocks!") 
